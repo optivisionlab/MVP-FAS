e074223a03e7f4828793fe28b385f763db23099b
optivision <optivision.work@gmail.com>
2026-02-06 13:36:03 +0700

    update parma backbone

--- models/make_network.py
+++ models/make_network.py
@@ -1,18 +1,18 @@
 import torch
 
 from models.MVP_FAS import mspt
-def get_network(cfg, net_name='MVP_FAS', device='cpu'):
+def get_network(cfg, net_name='MVP_FAS', device='cpu', backbone='ViT-B/16'):
     if net_name == 'MVP_FAS':
         print("net_name")
-        net = mspt(cfg, device=device)
+        net = mspt(cfg, device=device, backbone=backbone)
     # net = torch.nn.DataParallel(net).cuda()
     return net
 
-def set_pretrained_setting(net,optimizer,weight_path):
+def set_pretrained_setting(net, optimizer, weight_path):
     checkpoint_dict = torch.load(weight_path)
     checkpoint = checkpoint_dict['state_dict']
     optim_checkpoint = checkpoint_dict['optimizer']
-    last_epoch = checkpoint_dict['epoch']-1
+    last_epoch = checkpoint_dict['epoch'] - 1
     pretrained_dict = {k: v for k, v in checkpoint.items() if k in net.module.state_dict().keys()}
     net.module.load_state_dict(pretrained_dict)
     optimizer.load_state_dict(optim_checkpoint)
